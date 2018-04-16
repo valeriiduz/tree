82e00539f7e8916a66375d31dcbc5bb5b96421b7
Valerii Duz <duz.valera.od@ex.ua>
2018-04-16 15:23:19 +0300

    Created insertion attribute

---
+++ tree.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import hashlib
+import random
+
+class Tree:
+
+    pathToTree = os.path.dirname(os.path.abspath(__file__))
+    
+    def insert(self):
+        '''
+        Insert file to the data tree
+        '''
+        hashName = hashlib.sha256(bytes(int(random.random()*100000))).hexdigest()
+        hashDir = "fda/fadf"
+        dirPath = os.path.join(self.pathToTree, hashDir)
+        os.makedirs(dirPath)
+        output = open(os.path.join(dirPath, hashName), 'wb')
+
+    def rewrite(self, pathFile, fileName):
+        '''
+        Rewrite file in the data tree
+        '''
+        pas
+
+    def remove(self, pathFile):
+        '''
+        Remove file from the data tree
+        '''
+        os.remove(pathFile)
+
+    def show(self, pathFile):
+        '''
+        Show file in the data tree
+        '''
+        print(pathFile)
